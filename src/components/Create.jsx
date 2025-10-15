b55861a9501fb82d63656508c70989ea91443147
Dylan Daly <g00419061@atu.ie>
2025-10-15 14:13:46 +0000

    Lab 4

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,9 +1,54 @@
-const Create = () => { //sets create component
-    return (
-        <div>
-            <h3>Hello from the Create component</h3>
+import { useState } from "react";
+
+function Create() {
+  const [title, setTitle] = useState(''); //manages title information
+
+  const [year, setYear] = useState(''); //manages year information
+
+  const [poster, setPoster] = useState(''); //manages poster information
+
+  const handleSubmit = (e) => { //runs on form button press
+    e.preventDefault();
+    console.log(title, year, poster); //prints added movie information to console log
+  }
+
+  return (
+    <div>
+      <h2>This is my Create Component.</h2>
+      <form onSubmit={handleSubmit}>
+        {/*Form field for title*/}
+        <div className="form-group">
+          <label>Add Movie Title: </label> 
+          <input type="text"
+            className="form-control"
+            value={title}
+            onChange={(e) => { setTitle(e.target.value) }} //continuously updates as text entered into title field
+          />
         </div>
-    );
+
+        {/*Form field for year*/}
+        <div className="form-group">
+          <label>Add Movie Year: </label>
+          <input type="text"
+            className="form-control"
+            value={year}
+            onChange={(e) => { setYear(e.target.value) }} //continuously updates as text entered into year field
+          />
+        </div>
+
+        {/*Form field for poster*/}
+        <div className="form-group">
+          <label>Add Movie Poster: </label>
+          <textarea type="text"
+            className="form-control"
+            value={poster}
+            onChange={(e) => { setPoster(e.target.value) }} //continuously updates as text entered into poster field
+          />
+        </div>
+        <input type="submit" value="Add Movie" /> 
+      </form>
+    </div>
+  );
 }
 
-export default Create;
+export default Create;//marks page for export
--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -1,36 +1,28 @@
-import Movies from "./Movies"; //imports concent from Movies component
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Movies from "./Movies"; //imports content from Movies component
 
 const Read = () => { //sets read component
 
-    const data =
-        [
-            {
-                "Title": "Avengers: Infinity War",
-                "Year": "2018",
-                "imdbID": "tt4154756",
-                "Type": "movie",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-                "Title": "Captain America: Civil War",
-                "Year": "2016",
-                "imdbID": "tt3498820",
-                "Type": "movie",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            },
-            {
-                "Title": "World War Z",
-                "Year": "2013",
-                "imdbID": "tt0816711",
-                "Type": "movie",
-                "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
-            }
-        ];
+    const [myMovies, setMovie] = useState([]);
+
+    //React hook to excute code on change 
+    useEffect(
+        ()=>{
+            //do some http client work
+            axios.get('https://data-rep-mern-application.github.io/dataserver/movies.json') //gets data down from API
+            .then((response)=>{
+                console.log(response.myArray)//response to console log if data successfully returned
+                setMovie(response.data.myArray)//displays data to page if successfully returned
+            })
+            .catch((error)=>{console.log(error)});//response to console log if data not successfully returned
+        },[]
+    ); 
 
     return (
         <div>
             <h3>Hello from the Read component</h3>
-            <Movies myMovies = {data}></Movies> 
+            <Movies myMovies = {myMovies}></Movies> 
         </div> //retuns movie data to other .jsx pages
     );
 }
